9bcb0255c4264e6a165974f91a2ad90f131835b0
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-06-25 14:48:17 +0530

    chore: Add environment configuration for Supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ggawtpxrwzkvpstsbvle.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnYXd0cHhyd3prdnBzdHNidmxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzMDQ0MDEsImV4cCI6MjAzNDg4MDQwMX0.11GmDFgpHrVwrLL1DHq1exOSc7PBS4C0rvgQt-CKGSM',
+};
