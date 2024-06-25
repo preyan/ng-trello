dddc72a5e70c73333c334a9672246387b897c2a4
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-06-25 14:48:49 +0530

    feat: Add routes for login, workspace, and board components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './guards/auth.guard';
+import { BoardComponent } from './components/protected/board/board.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
+import { WorkspaceComponent } from './components/protected/workspace/workspace.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'workspace',
+    component: WorkspaceComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'workspace/:id',
+    component: BoardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
